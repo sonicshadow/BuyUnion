5cea969230b8a5857f4777ba71e9a1e76266d3b9
sonicshadow <sonic_458@126.com>
2017-09-29 18:01:04 +0800

    用户管理

--- BuyUnion/Controllers/UserManageController.cs
+++ BuyUnion/Controllers/UserManageController.cs
@@ -13,22 +13,21 @@ namespace BuyUnion.Controllers
         // GET: UserManage
         public ActionResult Index(Enums.UserType type = Enums.UserType.System, int page = 1)
         {
-            //var users =
-            //    from u in db.Users
-            //    from g in db.r
-            //   .Where(s => s.UserType == type)
-                
-            //    .Select(s => new UserViewModels
-            //    {
-            //        Id = s.Id,
-            //        UserName = s.UserName,
-            //        RegisterDateTime = s.RegisterDateTime,
-            //        UserType = s.UserType
-            //    })
-            //    .ToList();
-
-
-            return View();
+            var model =
+                (from u in db.Users
+                 join rg in db.RoleGroups on u.RoleGroupID equals rg.ID into rr
+                 from r in rr.DefaultIfEmpty()
+                 where u.UserType == type
+                 select new UserViewModels
+                 {
+                     Id = u.Id,
+                     UserName = u.UserName,
+                     RegisterDateTime = u.RegisterDateTime,
+                     UserType = u.UserType,
+                     RoleName = r.Name
+                 })
+                .ToPagedList(page);
+            return View(model);
         }
     }
 }
