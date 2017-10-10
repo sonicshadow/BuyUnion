d62a323ba0455a2bb7a061bd07b73a908061e82e
sonicshadow <sonic_458@126.com>
2017-10-10 11:32:38 +0800

    补充了缺失的基础类

--- BuyUnion/App_Start/BundleConfig.cs
+++ BuyUnion/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Linq;
+using System.Web;
 using System.Web.Optimization;
 
 namespace BuyUnion
@@ -26,6 +28,42 @@ namespace BuyUnion
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/comm").Include(
+                      "~/Scripts/Comm/canvas-to-blob.min.js",
+                      "~/Scripts/Comm/jsEx.js",
+                      "~/Scripts/Comm/jQueryEx.js",
+                      "~/Scripts/Comm/comm.js",
+                      "~/Scripts/Comm/check.js",
+                      "~/Scripts/Comm/uploadfile.js",
+                      "~/Scripts/Comm/imageResizeUpload.js",
+                      "~/Scripts/jquery.lazyload.min.js",
+                      "~/Scripts/Comm/imageModule.js",
+                      "~/Scripts/Comm/accessLog.js",
+                      "~/Scripts/Comm/urlMatch.js",
+                      "~/Scripts/Comm/getMessage.js"
+                  ));
+            bundles.Add(new ScriptBundle($"~/bundles/cloud").Include(
+              "~/Scripts/Comm/cloud.js"));
+            bundles.Add(new ScriptBundle($"~/bundles/searchUser").Include(
+              "~/Scripts/Comm/searchUser.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/swiper/css").Include(
+                    "~/Scripts/Swiper/css/swiper.min.css"));
+            bundles.Add(new ScriptBundle("~/bundles/swiper/js").Include(
+                "~/Scripts/Swiper/js/swiper.jquery.min.js",
+                "~/Scripts/Swiper/js/swiper.min.js"));
+
+            //view js
+            Action<string, string[]> addViewScripts = (name, js) =>
+            {
+                js = js.Select(s => s.Contains("~") ? s : $"~/Scripts/Views/{s}").ToArray();
+                bundles.Add(new ScriptBundle($"~/bundles/{name}").Include(js));
+            };
+
+            addViewScripts("ProductManageCreate", new string[] { "ProductManageCreate.js" });
+
         }
     }
 }
---
+++ BuyUnion/Models/ImageResizer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace BuyUnion.Models
+{
+    public class ImageResizer
+    {
+        public ImageResizer(string name, int width, int height, string image = null, int pWidth = 0, int pHeight = 0)
+        {
+            Name = name;
+            Width = width;
+            Height = height;
+            ImageUrl = image;
+            PreviewWidth = pWidth > 0 ? pWidth : width;
+            PreviewHeight = pHeight > 0 ? pHeight : height;
+        }
+
+        public string ImageUrl { get; set; }
+
+        private string pPreviewUrl;
+
+        public string PreviewUrl
+        {
+            get
+            {
+                if (pPreviewUrl == null)
+                {
+                    pPreviewUrl = string.IsNullOrWhiteSpace(ImageUrl)
+                        ? $"~/Content/Images/view/avatar_m@3x.png?w={PreviewWidth}&h={PreviewHeight}&scale=canvas&Bgcolor=f6f6f6"
+                        : Comm.ResizeImage(ImageUrl, PreviewWidth, PreviewHeight, image: Enums.DummyImage.Avatar);
+                }
+                return pPreviewUrl;
+            }
+        }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public string Name { get; set; }
+
+        public int PreviewWidth { get; set; }
+
+        public int PreviewHeight { get; set; }
+
+        public bool AutoInit { get; set; } = true;
+    }
+}
