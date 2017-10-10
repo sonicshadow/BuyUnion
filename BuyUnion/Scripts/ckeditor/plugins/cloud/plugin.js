60938681e6a8cb54f8afdf3ba71bf5b6249e4746
sonicshadow <sonic_458@126.com>
2017-10-10 09:18:11 +0800

    加入JS控件

---
+++ BuyUnion/Scripts/ckeditor/plugins/cloud/plugin.js
@@ -0,0 +1,34 @@
+﻿
+CKEDITOR.plugins.add('cloud',
+{
+    lang: 'zh-cn',
+    init: function (editor) {
+        var pluginName = 'cloud';
+        editor.ui.addButton('cloud',
+           {
+               label: editor.lang.cloud.cloud,
+               command: 'show',
+               icon: this.path + 'images/cloud.png'
+           });
+        var cmd = editor.addCommand('show', { exec: dalCloud });
+    }
+});
+
+if (cloud != undefined) {
+    var myCloud = new cloud("#cloud", {
+        added: function (data) {
+            var html = "";
+            $.each(data, function (i, n) {
+                html += "<p><img src='" + comm.imgFullUrl(n) + "' style='width:100%'/></p>";
+            });
+            html+="<p></p>"
+            editor.insertHtml(html);
+            myCloud.hide();
+        }
+    });
+
+    function dalCloud(e) {
+
+        //myCloud.show();
+    }
+}
---
+++ BuyUnion/Scripts/ckeditor/plugins/cyimage/plugin.js
@@ -0,0 +1,34 @@
+﻿
+var tempUploader = null;
+CKEDITOR.plugins.add('cyimage',
+{
+    lang: 'zh-cn',
+    init: function (editor) {
+        
+        var pluginName = 'cyimage';
+        editor.ui.addButton('cyimage',
+           {
+               label: editor.lang.cyimage.image,
+               command: 'show',
+               icon: this.path + 'images/image.png'
+           });
+        tempUploader = new uploader("#myModal_ckupload", {
+            closed: function (data) {
+                var html = "";
+                $.each(data, function (i, n) {
+                    html += "<p><img src='" + comm.imgFullUrl(n) + "' style='width:100%'/></p>";
+                });
+                html += "<p></p>"
+                editor.insertHtml(html);
+                tempUploader.clean();
+
+            }
+        });
+        var cmd = editor.addCommand('show', { exec: dalcyimage });
+    }
+});
+
+
+function dalcyimage(e) {
+    tempUploader.show();
+}
