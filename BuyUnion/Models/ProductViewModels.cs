5d1bce95ce0c79e9a287aaf6226ef592aa1d2dc1
sonicshadow <sonic_458@126.com>
2017-10-10 11:07:59 +0800

    添加了FileUpload

---
+++ BuyUnion/Models/ProductViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BuyUnion.Models
+{
+    public class ProductCreateEditViewModel : Product
+    {
+        [Display(Name = "主图片")]
+        public new FileUpload Image { get; set; } = new FileUpload { Max = 1, Type = FileType.Image };
+
+        [Display(Name = "次图片")]
+        public new FileUpload DetailsImage { get; set; } = new FileUpload { Max = 0, Type = FileType.Image };
+    }
+}
