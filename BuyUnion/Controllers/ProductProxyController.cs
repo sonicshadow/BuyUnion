a00c22bef742b0bd60bdd8dfd0fa64b0954ca9e2
LiangHuiWen123 <blueameng@163.com>
2017-10-11 18:06:50 +0800

    修改接口

--- BuyUnion/Controllers/ProductProxyController.cs
+++ BuyUnion/Controllers/ProductProxyController.cs
@@ -37,20 +37,27 @@ namespace BuyUnion.Controllers
             var pps = (from pp in db.ProductProxys
                        from p in db.Products
                        where pp.ProxyUserID == userId && p.ID == pp.ProductID
-                       select new { pp, p }).ToList().Select(s => new
-                       {
-                           s.pp.ProductID,
-                           s.p.Name,
-                           s.pp.ProxyUserID,
-                           s.pp.Count,
-                           s.pp.Max,
-                           s.pp.ID,
-                           Image = Comm.ResizeImage(s.p.Image, image: null),
-                       });
-            return Json(Comm.ToJsonResult("Success", "成功", pps), JsonRequestBehavior.AllowGet);
+                       select new { pp, p.Name, p.Image })
+                       .ToList().FirstOrDefault();
+            if (pps == null)
+            {
+                return Json(Comm.ToJsonResult("Error", "没有这个记录"), JsonRequestBehavior.AllowGet);
+            }
+            var data = new
+            {
+                pps.pp.ProductID,
+                pps.Name,
+                pps.pp.ProxyUserID,
+                pps.pp.Count,
+                pps.pp.Max,
+                pps.pp.ID,
+                Image = Comm.ResizeImage(pps.Image, image: null),
+            };
+            return Json(Comm.ToJsonResult("Success", "成功", data), JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
+        [AllowCrossSiteJson]
         public ActionResult Creat(ProductProxy model)
         {
             if (string.IsNullOrWhiteSpace(model.ProxyUserID))
@@ -67,7 +74,23 @@ namespace BuyUnion.Controllers
         }
 
         [HttpPost]
-        public ActionResult Dlete(int id)
+        [AllowCrossSiteJson]
+        public ActionResult Edit(ProductProxy model)
+        {
+            var pp = db.ProductProxys.FirstOrDefault(s => s.ID == model.ID);
+            if (pp == null)
+            {
+                return Json(Comm.ToJsonResult("Error", "没有参加此活动"));
+            }
+            pp.Max = model.Max;
+            pp.ProductID = model.ProductID;
+            db.SaveChanges();
+            return Json(Comm.ToJsonResult("Success", "成功"));
+        }
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public ActionResult Delete(int id)
         {
             var pp = db.ProductProxys.FirstOrDefault(s => s.ID == id);
             if (pp == null)
@@ -78,5 +101,6 @@ namespace BuyUnion.Controllers
             db.SaveChanges();
             return Json(Comm.ToJsonResult("Success", "成功"));
         }
+
     }
 }
