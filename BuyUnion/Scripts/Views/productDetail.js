5c1dba2753e4f73cd02aeeaf3aaab06cb2d4998b
zhongyingqiu <309058146@qq.com>
2017-10-16 15:34:25 +0800

    详情页-样式

--- BuyUnion/Scripts/Views/productDetail.js
+++ BuyUnion/Scripts/Views/productDetail.js
@@ -16,3 +16,46 @@ function GetParent() {
     });
 }
 GetParent();
+
+//swiper轮播
+var swiper = new Swiper('.productDetails-swiper .swiper-container', {
+    pagination: '.productDetails-swiper .swiper-pagination',
+    paginationClickable: true
+});
+
+//图文详情
+$("[name='slide'] .title").click(function () {
+    $(this).parent().find(".detail").slideToggle();
+    $(this).toggleClass("active");
+});
+
+//规格
+$("#speclist li").click(function () {
+    $("#speclist li").removeClass("active");
+    $(this).addClass("hidden");
+});
+
+//数量
+var plus = $("#plus");
+var minus = $("#minus");
+var sumBox = $("input[name='buynum']");
+var sum = sumBox.val();
+plus.click(function () {
+    sum++;
+    minus.removeClass("minus-disable");
+    sumBox.val(sum);
+});
+
+minus.click(function () {
+    if (sum == "1") {
+        minus.addClass("minus-disable");
+    } else {
+        sum--;
+
+        if (sum == "1") {
+            minus.addClass("minus-disable");
+        }
+    }
+    
+    sumBox.val(sum);
+});
