bc58e0414ccbd6cb8a3397df2ede3e7f6f8fdb6b
zhongyingqiu <309058146@qq.com>
2017-10-27 11:03:30 +0800

    主页-图片压缩，pc适应

--- BuyUnion/Scripts/Views/homeIndex.js
+++ BuyUnion/Scripts/Views/homeIndex.js
@@ -2,3 +2,21 @@
 var swiper = new Swiper('.homeIndexM-swiper .swiper-container', {
     direction: 'vertical'
 });
+
+var uat = navigator.userAgent;
+var isWX = (uat.toLowerCase().match(/MicroMessenger/i) == 'micromessenger');
+var isiOS = !!uat.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+var isIosWebkit = !!window.webkit && !isWX;
+var isWebkit = !!window.webkit;
+var isAndroid = !!window.jsCallNative;
+var isApp = isAndroid || isIosWebkit;
+
+$("#btns").click(function () {
+    if (isWX || !isiOS) {
+        $(".linkToMobile-tip").removeClass("hidden");
+    }
+});
+
+$("#closed").click(function () {
+    $(".linkToMobile-tip").addClass("hidden");
+})
