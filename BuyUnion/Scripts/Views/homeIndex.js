c4a91ce87aacf4748e5b06b63b058595a8b5d9f4
sonicshadow <sonic_458@126.com>
2017-10-27 11:05:20 +0800

    二维码

--- BuyUnion/Scripts/Views/homeIndex.js
+++ BuyUnion/Scripts/Views/homeIndex.js
@@ -3,10 +3,10 @@ var swiper = new Swiper('.homeIndexM-swiper .swiper-container', {
     direction: 'vertical'
 });
 
-//if (!new check().isWeiXin()) {
-//    $(".homeIndexM-pc").removeClass("hidden");
-  
-//} else {
-//    $(".homeIndexM").removeClass("hidden");
-//}
-$(".homeIndexM").removeClass("hidden");
+if (new check().isWeiXin() || location.getUrlParam("test") == "1") {
+
+    $(".homeIndexM").removeClass("hidden");
+}
+else {
+    $(".homeIndexM-pc").removeClass("hidden");
+}
