68f1026380891bbb49db05eb462a09c841a95aaa
LiangHuiWen123 <blueameng@163.com>
2017-10-27 10:03:07 +0800

    生成二維碼

---
+++ BuyUnion/Controllers/QrCodeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing;
+using System.Text;
+
+
+namespace BuyUnion.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        [HttpGet]
+        public FileResult Index(string data)
+        {
+            var steam = new System.IO.MemoryStream();
+            Image image = QrCode.Generate(data);
+            image.Save(steam, System.Drawing.Imaging.ImageFormat.Jpeg);
+            image.Dispose();
+            return File(steam.ToArray(), "image/jpeg");
+        }
+    }
+}
